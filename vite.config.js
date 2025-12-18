5a485b2707eab30adae46eb3223575b7c54b296d
jewells <jewellsco@gmail.com>
2025-12-18 14:04:21 -0700

    Simplify Vite config to ensure build stability

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,22 +8,22 @@ export default defineConfig({
         outDir: 'dist',
         rollupOptions: {
             input: {
-                main: resolve(__dirname, 'index.html'),
-                aau: resolve(__dirname, 'aau.html'),
-                about: resolve(__dirname, 'about.html'),
-                academy: resolve(__dirname, 'academy.html'),
-                calendarEmbed: resolve(__dirname, 'calendar-embed.html'),
-                calendarPreview: resolve(__dirname, 'calendar-preview.html'),
-                cardPreview: resolve(__dirname, 'card-preview.html'),
-                coachPortal: resolve(__dirname, 'coach-portal.html'),
-                contact: resolve(__dirname, 'contact.html'),
-                parentAudit: resolve(__dirname, 'parent-audit.html'),
-                parentPortal: resolve(__dirname, 'parent-portal.html'),
-                parents: resolve(__dirname, 'parents.html'),
-                product: resolve(__dirname, 'product.html'),
-                shop: resolve(__dirname, 'shop.html'),
-                skillAudit: resolve(__dirname, 'skill-audit.html'),
-                training: resolve(__dirname, 'training.html'),
+                main: 'index.html',
+                aau: 'aau.html',
+                about: 'about.html',
+                academy: 'academy.html',
+                calendarEmbed: 'calendar-embed.html',
+                calendarPreview: 'calendar-preview.html',
+                cardPreview: 'card-preview.html',
+                coachPortal: 'coach-portal.html',
+                contact: 'contact.html',
+                parentAudit: 'parent-audit.html',
+                parentPortal: 'parent-portal.html',
+                parents: 'parents.html',
+                product: 'product.html',
+                shop: 'shop.html',
+                skillAudit: 'skill-audit.html',
+                training: 'training.html',
             },
         },
     },
